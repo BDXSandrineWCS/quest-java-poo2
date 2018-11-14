411c492a7103351a9128e1d8109cc1a100b43f21
sandrine <poupardsandrine@gmail.com>
2018-11-14 20:48:12 +0100

    14112018 Solution 1 POO2

---
+++ Hangar.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Hangar{
+	
+	public static void main( String ... args ) {
+		Car clio = new Car( "Renault" , 60000 );
+		Car mini = new Car( "BMW" , 150000 );
+		Car ignis = new Car( "Suzuki" , 107004 );
+		Boat schooner = new Boat ( "Aegean", 2007090);
+		Boat peterson = new Boat ( "Camden", 8098);
+		Boat beaufort = new Boat ( "Herbulot", 403465);
+		ArrayList<Vehicle> vehicles = new ArrayList<>();
+        	/*vehicles.add(new Car( "Renault" , 60000 ));
+        	vehicles.add(new Boat ( "Aegean", 2007090));
+		vehicles.add(new Boat ( "Camden", 8098));
+		vehicles.add(new Car( "BMW" , 150000 ));
+		vehicles.add(new Boat ( "Herbulot", 403465));
+		vehicles.add(new Car( "Suzuki" , 107004 ));*/
+		vehicles.add(clio);
+        	vehicles.add(schooner);
+		vehicles.add(peterson);
+		vehicles.add(mini);
+		vehicles.add(beaufort);
+		vehicles.add(ignis);
+
+		for (Vehicle vehicle : vehicles) {
+			System.out.println(vehicle.doStuff());
+		}
+		
+	}
+}
